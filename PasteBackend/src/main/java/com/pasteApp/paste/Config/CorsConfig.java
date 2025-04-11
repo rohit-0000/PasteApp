171bb0fdd68f9bbca43b94b43d35b06e05d4c1cd
rohit-0000 <rohit20040121@gmail.com>
2025-04-11 19:05:10 +0530

    adding paste app

--- PasteBackend/src/main/java/com/pasteApp/paste/Config/CorsConfig.java
+++ PasteBackend/src/main/java/com/pasteApp/paste/Config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.pasteApp.paste.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -12,13 +13,20 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${frontend.url}")
+    String frontEndUrl1;
+    @Value("${frontend.url2}")
+    String frontEndUrl2;
+    @Value("${frontend.local}")
+    String frontEndLocal;
+
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE) // Ensure it runs early
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(frontEndLocal,frontEndUrl1,frontEndUrl1));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         source.registerCorsConfiguration("/**", config);
